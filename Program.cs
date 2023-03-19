6e50e51501c4e04aa3afb0ecd528db88e67f9aa8
NiButera <nikolasxp683@gmail.com>
2023-03-19 01:48:57 -0300

    Fiz um programa que pede para clicar para continuar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿#pragma warning disable
+
+Console.Beep(1320, 400);
+Console.WriteLine();
+Console.WriteLine("Ola! Pressione qualquer tecla para continuar");
+Console.ReadKey(true);
+
+Console.Beep(4320, 400);
+Console.WriteLine("Obrigado!");
+Console.WriteLine();
